0526b38c8f32878c9adf7e83e72685083ede7952
Douglas S. Correa <me@douglascorrea.io>
2017-11-29 20:01:04 -0200

    Simple UI static unique component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Please insert a Country Fifa code to check Match results</h1>
+                </header>
+                <div className="App-container">
+                    <form>
+                        <input type="text"/>
+                    </form>
+                    <div className="Matches-Results">
+                        <ul>
+                            <li>USA [1] x [0] AUS</li>
+                            <li>BRA [1] x [0] ARG</li>
+                            <li>BRA [1] x [0] ARG</li>
+                            <li>BRA [1] x [0] ARG</li>
+                            <li>BRA [1] x [0] ARG</li>
+                            <li>BRA [1] x [0] ARG</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
