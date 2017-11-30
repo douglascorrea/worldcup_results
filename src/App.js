c6a346f79f9f11e59d37f47284f3bb6483a555be
Douglas S. Correa <me@douglascorrea.io>
2017-11-30 13:25:42 -0200

    Fix some parts of layout

--- src/App.js
+++ src/App.js
@@ -10,10 +10,6 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Please insert a Country Fifa code to check Match results</h1>
-                </header>
                 <div className="App-container">
                     <div className="Matches-Results">
                         <Message />
