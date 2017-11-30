9883601318a81d875446bc2a759af4fd98bcf300
Douglas S. Correa <me@douglascorrea.io>
2017-11-29 23:36:33 -0200

    Handling 404 responses from server

--- src/api/resultsServices.js
+++ src/api/resultsServices.js
@@ -1,4 +1,25 @@
+const checkStatus = (response) => {
+    if (response.status >= 200 && response.status < 300) {
+        return response
+    } if (response.status === 404) {
+        // handling wrong country code
+        response.json = () => [];
+        return response;
+    }
+    else {
+        const error = new Error(response.statusText);
+        error.response = response;
+        throw error
+    }
+};
+
+const parseJson = (res) => res.json();
+
 export const getMatches = (countryCode) => {
     return fetch(`http://localhost:8080/${countryCode}`)
-        .then(res => res.json())
+        .then(checkStatus)
+        .then(parseJson)
+        .catch((error) => {
+            console.log('request failed a', error)
+        })
 };
--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
 import {applyMiddleware, createStore} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import reducer from './reducers/results'
 
 export default createStore(
     reducer,
-    applyMiddleware(thunk)
+    composeWithDevTools(applyMiddleware(thunk))
 )
