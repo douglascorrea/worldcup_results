4f120af0849c4d6d496bd7684cb9eb7d8bbfedcd
Douglas S. Correa <me@douglascorrea.io>
2017-11-30 15:46:39 -0200

    Addded redux-mock-store to test connected components

--- src/components/CountryForm.test.js
+++ src/components/CountryForm.test.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import {Provider} from 'react-redux'
-import {shallow} from 'enzyme';
-import ConnectedCountryForm, { CountryForm } from './CountryForm';
+import {shallow, mount, render} from 'enzyme';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import ConnectedCountryForm, {CountryForm} from './CountryForm';
 
+const mockStore = configureMockStore( [ thunk ]);
+const storeStateMock = {
+    result: {
+        currentSearch: 'USA'
+    }
+};
+
+describe('ContryForm', () => {
 // shallow NOT connected rendering tests
-it('renders without crashing', () => {
-    shallow(<CountryForm />);
-});
+    it('renders without crashing', () => {
+        shallow(<CountryForm/>);
+    });
 
-it('checking receiving props on correct input', () => {
-    const wrapper = shallow(<CountryForm currentSearch='Search' />);
-    expect(wrapper.find('input')).toHaveValue('Search');
-});
+    it('checking receiving props on correct input', () => {
+        const wrapper = shallow(<CountryForm currentSearch='Search'/>);
+        expect(wrapper.find('input')).toHaveValue('Search');
+    });
 
+// connected comppnent
+    it('render connected component',() => {
+        const store = mockStore(storeStateMock);
+        const wrapper = shallow(<ConnectedCountryForm store={store}/>).dive();
+        expect(wrapper.find('input')).toHaveValue('USA');
+    })
+});
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,7 @@ import {configure} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import 'jest-enzyme'
 
+console.error = message => {
+  throw new Error(message);
+};
 configure({adapter: new Adapter()});
